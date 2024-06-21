62333e4b0f9166c5d8138edf579b426e69f4b098
Frederick Morrison <frederick.morrison@outlook.com>
2024-06-20 21:01:57 -0600

    add Java version information to output

---
+++ src/Client.java
@@ -0,0 +1,8 @@
+public class Client
+{
+    private int customerID;
+    private String name;
+    private Dates arrive;
+    private Dates leave;
+    private Room bedroom;
+}
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,19 @@ public class Main
 {
     public static void main(String[] args)
     {
+        System.out.format("Java version: %s%n", getJavaVersion());
+
         Dates d1 = new Dates(1, 2, 2024);
         Dates d2 = new Dates(1, 3, 2024);
         System.out.format("Difference in days: %d%n", Dates.StayDays(d1, d2));
+    }
+
+    /** get the java version that is running the current program
+     * @return string containing the java version running the current program
+     */
+    private static String getJavaVersion()
+    {
+        Runtime.Version runTimeVersion = Runtime.version();
+        return String.format("%s.%s.%s.%s", runTimeVersion.feature(), runTimeVersion.interim(), runTimeVersion.update(), runTimeVersion.patch());
     }
 }
